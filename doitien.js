89346a0164af0a97bdc3ea4ab23fde55624c0883
lê đoàn <huongly95967@gmail.com>
2022-12-10 22:56:27 +0700

    btb7

---
+++ doitien.js
@@ -0,0 +1,16 @@
+function doit() {
+
+
+    let a = document.getElementById('amount').value;
+    let b = document.getElementById('from').value;
+    let c = document.getElementById('to').value;
+    if (c == b) {
+        alert(a);
+    }
+    if (b == "VND" && c == "USD") {
+        alert(a / 24000 + "$")
+    }
+    if (b == "USD" && c == "VND") {
+        alert(a * 24000 + "đ")
+    }
+}
